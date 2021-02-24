1b2702e07b554d0bb024c869896cef06362b4f4b
Gernot Hillier <gernot.hillier@siemens.com>
2021-02-24 09:21:44 +0100

    feat(sw360_objects): add package URL support

--- tests/test_sw360obj_component.py
+++ tests/test_sw360obj_component.py
@@ -45,7 +45,39 @@ class Sw360ObjTestComponent(Sw360ObjTestBase):
         self.assertEqual(comp.name, "acl")
         self.assertEqual(comp.details["somekey"], "value")
         self.assertEqual(len(comp.releases), 1)
+        self.assertEqual(len(comp.purls), 0)
         self.assertEqual(comp.releases["7c4"].component_id, "123")
+
+    @responses.activate
+    def test_get_component_with_purls(self):
+        responses.add(
+            responses.GET,
+            SW360_BASE_URL + "components/123",
+            json={
+                'name': 'acl',
+                'somekey': 'value',
+                'externalIds': {
+                    'package-url': 'pkg:deb/debian/acl@1.5.43 pkg:deb/ubuntu/acl@1.5.43-ub0'}})
+        comp = Component().get(self.lib, "123")
+        self.assertEqual(len(comp.purls), 2)
+        self.assertEqual(comp.purls[0].name, "acl")
+        self.assertNotIn("package-url", comp.external_ids)
+
+    @responses.activate
+    def test_get_component_invalid_purls(self):
+        responses.add(
+            responses.GET,
+            SW360_BASE_URL + "components/123",
+            json={
+                'name': 'acl',
+                'somekey': 'value',
+                'externalIds': {
+                    'package-url': 'pkg:xxx@0.43',
+                    'purl': 'pkg:deb/debian/acl@1.5.43'}})
+        comp = Component().get(self.lib, "123")
+        self.assertEqual(len(comp.purls), 1)
+        self.assertEqual(comp.external_ids['package-url'], 'pkg:xxx@0.43')
+        self.assertEqual(comp.purls[0].name, "acl")
 
 
 if __name__ == "__main__":

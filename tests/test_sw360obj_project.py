bc639dbd3085ac6018dcee972364dd09e1d34300
Gernot Hillier <gernot.hillier@siemens.com>
2021-02-24 09:21:44 +0100

    feat(sw360_objects): add package URL support

--- tests/test_sw360obj_project.py
+++ tests/test_sw360obj_project.py
@@ -49,6 +49,48 @@ class Sw360ObjTestProject(Sw360ObjTestBase):
 
         self.assertEqual(str(proj), "MyProj 11.0 (123)")
 
+    @responses.activate
+    def test_get_project_extid(self):
+        responses.add(
+            responses.GET,
+            SW360_BASE_URL + "projects/123",
+            json={
+                'name': 'MyProj',
+                'version': '11.0',
+                'externalIds': {'some.id': '7105'}})
+        proj = Project().get(self.lib, "123")
+        self.assertEqual(proj.external_ids["some.id"], "7105")
+        self.assertEqual(len(proj.purls), 0)
+
+    @responses.activate
+    def test_get_project_purl_string(self):
+        responses.add(
+            responses.GET,
+            SW360_BASE_URL + "projects/123",
+            json={
+                'name': 'App',
+                'version': '11.0',
+                'externalIds': {
+                  'package-url': 'pkg:deb/debian/app@11.0-1?arch=source'}})
+        proj = Project().get(self.lib, "123")
+        self.assertEqual(len(proj.purls), 1)
+        self.assertEqual(proj.purls[0].name, "app")
+        self.assertEqual(proj.purls[0].version, "11.0-1")
+
+    @responses.activate
+    def test_get_project_purl_invalid(self):
+        responses.add(
+            responses.GET,
+            SW360_BASE_URL + "projects/123",
+            json={
+                'name': 'App',
+                'version': '11.0',
+                'externalIds': {
+                  'package-url': 'pkg:huhu'}})
+        proj = Project().get(self.lib, "123")
+        self.assertEqual(len(proj.purls), 0)
+        self.assertEqual(proj.external_ids["package-url"], "pkg:huhu")
+
 
 if __name__ == "__main__":
     unittest.main()

955e8352c6062fc9e65e58b0b9302526d7061244
Thomas Graf <tngraf@gmx.de>
2021-05-27 18:52:06 +0200

    docs: update sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -12,13 +12,17 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../sw360'))
+current_dir = os.path.dirname(__file__)
+target_dir = os.path.abspath(os.path.join(current_dir, "../sw360"))
+sys.path.insert(0, target_dir)
+target2 = os.path.abspath(os.path.join(current_dir, "../"))
+sys.path.insert(0, target2)
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'Python interface for SW360'
-copyright = '2020, Thomas Graf'
+copyright = '2020, Siemens AG'
 author = 'Thomas Graf'
 
 
@@ -27,7 +31,7 @@ author = 'Thomas Graf'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -35,7 +39,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -44,8 +48,18 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+html_theme_options = {
+    "github_user": "sw360",
+    "github_repo": "sw360python",
+    "github_button": True,
+    "fixed_sidebar": True,
+    "extra_nav_links": {
+        "Package on PyPi": "https://pypi.org/project/sw360/",
+        "Source on Github": "https://github.com/sw360/sw360python/",
+    },
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']

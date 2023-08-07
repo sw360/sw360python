132ad31c4d82dd04c3f03c195cb722d1f9746f6b
Thomas Graf <tngraf@gmx.de>
2023-08-07 22:57:29 +0200

    chores: show checks that are run

--- RunChecks.ps1
+++ RunChecks.ps1
@@ -7,8 +7,13 @@
 
 # 2022-07-15, T. Graf
 
+Write-Host "flake8 ..."
 poetry run flake8
+
+Write-Host "markdownlint ..."
 npx -q markdownlint-cli *.md
+
+Write-Host "done."
 
 
 # -----------------------------------

3e78d411e0448ce8312d9a5fa8d184b21df07add
Gernot Hillier <gernot.hillier@siemens.com>
2025-01-29 21:36:56 +0100

    sw360_objects: clear interface for get and update, fix deduplication

--- tests/test_sw360obj_release.py
+++ tests/test_sw360obj_release.py
@@ -15,7 +15,7 @@ from sw360 import Release
 
 class Sw360ObjTestRelease(Sw360ObjTestBase):
     def test_repr(self):
-        r = Release(release_id="123", name="TestCmp", version="1.4",
+        r = Release(id_="123", name="TestCmp", version="1.4",
                     downloadurl="http://www")
 
         r = eval(repr(r))
@@ -42,7 +42,7 @@ class Sw360ObjTestRelease(Sw360ObjTestBase):
                 '_links': {
                     'sw360:component': {
                         'href': SW360_BASE_URL + 'components/7b4'}}})
-        r = Release().get(self.lib, "123")
+        r = Release.get(self.lib, "123")
         self.assertEqual(r.name, "acl")
         self.assertEqual(r.details["somekey"], "value")
         self.assertEqual(len(r.purls), 0)
@@ -57,7 +57,7 @@ class Sw360ObjTestRelease(Sw360ObjTestBase):
                 'name': 'acl',
                 'version': '1.4',
                 'externalIds': {'some.id': '7105'}})
-        r = Release().get(self.lib, "123")
+        r = Release.get(self.lib, "123")
         self.assertEqual(r.external_ids["some.id"], "7105")
         self.assertEqual(len(r.purls), 0)
 
@@ -71,7 +71,21 @@ class Sw360ObjTestRelease(Sw360ObjTestBase):
                 'version': '1.4',
                 'externalIds': {
                   'package-url': 'pkg:deb/debian/linux@4.19.98-1?arch=source'}})
-        r = Release().get(self.lib, "123")
+        r = Release.get(self.lib, "123")
+        self.assertEqual(len(r.purls), 1)
+        self.assertEqual(r.purls[0].name, "linux")
+        self.assertEqual(r.purls[0].version, "4.19.98-1")
+
+        r = Release.get(self.lib, "123")
+        self.assertEqual(len(r.purls), 1)
+
+        responses.replace(
+            responses.GET,
+            SW360_BASE_URL + "releases/123",
+            json={
+                'name': 'acl',
+                'version': '1.4'})
+        r = Release.get(self.lib, "123")
         self.assertEqual(len(r.purls), 1)
         self.assertEqual(r.purls[0].name, "linux")
         self.assertEqual(r.purls[0].version, "4.19.98-1")
@@ -86,7 +100,7 @@ class Sw360ObjTestRelease(Sw360ObjTestBase):
                 'version': '1.4',
                 'externalIds': {
                   'package-url': 'pkg:huhu'}})
-        r = Release().get(self.lib, "123")
+        r = Release.get(self.lib, "123")
         self.assertEqual(len(r.purls), 0)
         self.assertEqual(r.external_ids["package-url"], "pkg:huhu")
 
@@ -102,7 +116,7 @@ class Sw360ObjTestRelease(Sw360ObjTestBase):
                   'package-url': [
                     'pkg:deb/debian/linux@4.19.98-1?arch=source',
                     'pkg:deb/debian/linux-signed-amd64@4.19.98%2B1?arch=source']}})
-        r = Release().get(self.lib, "123")
+        r = Release.get(self.lib, "123")
         self.assertEqual(len(r.purls), 2)
         self.assertEqual(r.purls[1].name, "linux-signed-amd64")
         self.assertEqual(r.purls[1].version, "4.19.98+1")
@@ -119,7 +133,7 @@ class Sw360ObjTestRelease(Sw360ObjTestBase):
                 'externalIds': {
                   'package-url': '["pkg:deb/debian/linux@4.19.98-1?arch=source",'
                                  ' "pkg:deb/debian/linux-signed-amd64@4.19.98%2B1?arch=source"]'}})
-        r = Release().get(self.lib, "123")
+        r = Release.get(self.lib, "123")
         self.assertEqual(len(r.purls), 2)
         self.assertEqual(r.purls[1].name, "linux-signed-amd64")
         self.assertEqual(r.purls[1].version, "4.19.98+1")

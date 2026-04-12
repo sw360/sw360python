dd4448547df649c72bd1b248a2203591a1e290e5
Thomas Graf <tngraf@gmx.de>
2026-04-12 19:04:31 +0200

    feat: release version 1.11.0

--- sw360/__init__.py
+++ sw360/__init__.py
@@ -1,5 +1,5 @@
 # -------------------------------------------------------------------------------
-# Copyright (c) 2019-2025 Siemens
+# Copyright (c) 2019-2026 Siemens
 # All Rights Reserved.
 # Author: thomas.graf@siemens.com
 #
@@ -7,7 +7,7 @@
 # SPDX-License-Identifier: MIT
 # -------------------------------------------------------------------------------
 
-__version__ = (1, 10, 1)
+__version__ = (1, 11, 0)
 
 from .sw360_api import SW360
 from .sw360error import SW360Error
